ebae114b3615eb55d033b49ff29b9a9de4b981d1
Ihebowski <iheb.barrah@gmail.com>
2024-04-28 14:39:30 +0100

    Create the project structure.

---
+++ android/app/src/main/java/dev/ihebowski/lightsync/lightsync/MainActivity.java
@@ -0,0 +1,6 @@
+package dev.ihebowski.lightsync.lightsync;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lightsync/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LightSync',
+      theme: ThemeData(
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
